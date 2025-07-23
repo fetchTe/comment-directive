32a5bbf76a38e12aacdb727d760c07ae7eed8514
te <git@te.wtf>
2025-07-23 10:59:38 -0500

    refactor(bin): node first bin with quickjs directives

--- src/bin.ts
+++ src/bin.ts
@@ -1,4 +1,9 @@
-// ###[IF]node=1;sed=/std.err.puts/process.stderr.write/g;
+/**                                                                       @about
+@desc: bin for both quickjs and npm bin; uses itself to build quickjs compliant
+       version of comment-directive which is used to build the executables
+***                                                                           */
+// ###[IF]node=1;sed=/process.stderr.write/std.err.puts/g;
+/* eslint-disable @stylistic/max-len */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import {getMeta} from './_macro_' with { type: 'macro'};
 import {
@@ -8,7 +13,7 @@ import {
   prettyPrintJson,
   getArgv,
   getBooly,
-  // paint,
+  getArgvPositional,
 } from './bin.utils.ts';
 import {
   DEFAULT_OPTIONS,
@@ -18,17 +23,22 @@ import {
   extensions,
 } from './lang.ts';
 import type {
-  CommentLang,
+  CommentRegexOption,
 } from './lang.ts';
 import type {
   CommentOptions,
 } from './index.ts';
-
-// ###[IF]node=1;rm=3L;
+// ###[IF]node=1;un=comment;
+/*
 // quickJS-NG standard library modules
 import * as os from 'qjs:os';
 import * as std from 'qjs:std';
+*/
+// ###[IF]node=1;rm=3L;
+import * as fs from 'node:fs';
+import * as path from 'node:path';
 
+type CommentDirectives = Record<string, boolean | number | string>;
 
 type Options = {
   // cli
@@ -48,8 +58,13 @@ type Struc = {
   ctx: Options;
 };
 
+// ###[IF]node=1;un=comment;
+/*
 // @ts-expect-error 'in' is a resvered key so can't defined in d.ts
 const IS_PIPED = !os.isatty(std.in.fileno());
+*/
+// ###[IF]node=1;rm=1L;
+const IS_PIPED = !process.stdin.isTTY;
 
 const CMT_OPTIONS_BOOL = [
   'escape',
@@ -75,41 +90,94 @@ const istruc: Struc  = {
 };
 
 
-type CommentDirectives = Record<string, boolean | number | string>;
+const ensureDirSync = (filePath: string, verbose = false) => {
+  verbose && console.log(`[INFO] ensureDirSync: ${filePath}`);
+  // ###[IF]node=1;rm=2L;
+  if (fs.existsSync(path.dirname(filePath))) { return; }
+  fs.mkdirSync(path.dirname(filePath), {recursive: true});
+  // ###[IF]node=1;un=comment;
+  /*
+  if (filePath.includes('..')) {
+    verbose && console.log(`[INFO] ensureDirSync:skip: relative path`);
+    return;
+  }
+  const lastSlashIndex = filePath.lastIndexOf('/');
+  // ignore root paths like '/file.js'
+  if (lastSlashIndex < 1) {
+    verbose && console.log(`[INFO] ensureDirSync:skip: root path`);
+    return;
+  }
+  // pretty rudimentary but i aint dinkin around with all the edge cases (cough windows)
+  const idirc = filePath.substring(0, lastSlashIndex);
+  verbose && console.log(`[INFO] ensureDirSync:idirc: ${idirc}`);
+  const parts = idirc.split('/');
+  let ipath = parts[0];
+  for (let i = 1; i < parts.length; i++) {
+    ipath += '/' + parts[i];
+    verbose && console.log(`[INFO] ensureDirSync:ipath: ${ipath}`);
+    if (!ipath) { continue; }
+    // attempt to create the directory - returns a negative errno on failure
+    const ret = os.mkdir(ipath, 0o777);
+    if (ret < 0 && ret !== -std.Error.EEXIST) {
+      verbose && std.err.puts(`[ERRO] ensureDirSync: ${ret}\n`);
+      throw new Error(`Failed to create directory '${ipath}': ${std.strerror(-ret)}`);
+    }
+  }
+  */
+};
 
-const cli = ({ctx, options, directives}: Struc): boolean => {
+
+const cli = async ({ctx, options, directives}: Struc): Promise<boolean> => {
   try {
-    const {input, output, overwrite, dry, verbose} = ctx;
-    // @ts-expect-error 'in' is a resvered key so can't defined in d.ts
-    let content: string | null = IS_PIPED ? std.in.readAsString() : null;
+    const {input, overwrite, dry, verbose} = ctx;
+    const output = ctx.output ? ctx.output : (overwrite ? input : null);
+    let content: string | null = null;
+    if (IS_PIPED) {
+      // ###[IF]node=1;rm=1L;
+      for await (const chunk of process.stdin) { content += chunk; }
+      // ###[IF]node=1;un=comment;
+      /*
+      // @ts-expect-error 'in' is a resvered key so can't defined in d.ts
+      for await (const chunk of std.in.readAsString()) { content += chunk; }
+      */
+    }
+
     if (!input && !content) {
-      std.err.puts('[ERRO] no input file argument or piped stdin\n');
-      std.err.puts(`     > for usage information: '${getMeta().name} --help'\n`);
+      process.stderr.write('[ERRO] no input file argument or piped stdin\n');
+      process.stderr.write(`     > for usage information: '${getMeta().name} --help'\n`);
       return false;
     }
+
+    // check if target exists
     if (input && !content) {
-      // check if target exists
-      const [stat, statErr] = os.stat(input);
-      if (statErr !== 0) {
-        std.err.puts(`[ERRO] cannot access input file: '${input}'\n`);
-        ctx.verbose && std.err.puts('[ERRO:INFO] ' + std.strerror(-statErr));
+      // ###[IF]node=1;un=comment;
+      // const [stat, statErr] = os.stat(input);
+      // ###[IF]node=1;sed=/!fs.existsSync(input)/statErr !== 0/;
+      if (!fs.existsSync(input)) {
+        process.stderr.write(`[ERRO] cannot access input file: '${input}'\n`);
+        // ###[IF]node=1;un=comment;
+        // ctx.verbose && process.stderr.write('[ERRO:INFO] ' + std.strerror(-statErr));
         return false;
       }
-      if ((stat.mode & os.S_IFMT) !== os.S_IFREG) {
-        std.err.puts(`[ERRO] Input path '${input}' is not a regular file.\n`);
+      // ###[IF]node=1;sed=/!fs.statSync(input)?.isFile()/(stat.mode & os.S_IFMT) !== os.S_IFREG/;
+      if (!fs.statSync(input)?.isFile()) {
+        process.stderr.write(`[ERRO] Input path '${input}' is not a regular file\n`);
         return false;
       }
 
       // read file content
-      content = std.loadFile(input, {binary: false}) as string | null;
+      // ###[IF]node=1;rm=1L;
+      content = fs.readFileSync(input, 'utf-8');
+      // ###[IF]node=1;un=comment;
+      // content = std.loadFile(input, {binary: false}) as string | null;
       if (typeof content !== 'string') {
-        std.err.puts(`[ERRO] Failed to read file "${input}"`);
+        process.stderr.write(`[ERRO] Failed to read file "${input}"`);
         return false;
       }
     }
 
     if (typeof content !== 'string') {
-      std.err.puts(`[ERRO] Failed to read/parse input... should never happen`);
+      process.stderr.write(`[ERRO] Failed to read/parse input... should never happen`);
       return false;
     }
 
@@ -125,38 +193,40 @@ const cli = ({ctx, options, directives}: Struc): boolean => {
 
     if (!output) {
       // output to stdout
-      std.out.puts(result);
+      // ###[IF]node=1;sed=/process.stdout.write/std.out.puts/;
+      process.stdout.write(result);
       // console.log(result);
       return true;
     }
 
-    // Check if output file exists and handle overwrite
-    const [_outputStatResult, outputStatErr] = os.stat(output);
-    if (outputStatErr === 0 && !overwrite) {
-      std.err.puts(`[ERRO] Output file "${output}" already exists. Use --overwrite to overwrite.`);
+    // ensures no foot is blown off; otherwise trust the output is true
+    if (input && (input === output) && !overwrite) {
+      process.stderr.write(`[ERRO] Need to use --overwrite to overwrite the input (!DANGER-ZONE!)`);
       return false;
     }
 
-    // Write to output file
+    // write to output file
     try {
+      ensureDirSync(output, verbose);
       verbose && console.log(`[INFO] writing to: ${output}`);
-      std.writeFile(output, result);
+      // ###[IF]node=1;sed=/fs.writeFileSync/std.writeFile/;
+      fs.writeFileSync(output, result);
       verbose && console.log(`[INFO] wrote to  : ${output}`);
       return true;
     } catch (err) {
-      std.err.puts(`[ERRO:WRITE] Failed to write to "${output}": `
+      process.stderr.write(`[ERRO:WRITE] Failed to write to "${output}": `
                     + (((err instanceof Error) ? err.message : null) ?? 'unknown error'));
       ctx.verbose
-        && std.err.puts('[STACK:TRACE]\n' + ((err as any)?.stack ?? 'no stack trace available'));
+        && process.stderr.write('[STACK:TRACE]\n' + ((err as any)?.stack ?? 'no stack trace available'));
       return false;
     }
 
   } catch (err) {
     const errMsg = '[ERRO:CATCH] '
       + (((err instanceof Error) ? err.message : null) ?? 'unknown error');
-    std.err.puts(errMsg);
+    process.stderr.write(errMsg);
     ctx.verbose
-       && std.err.puts('[STACK:TRACE]\n' + ((err as any)?.stack ?? 'no stack trace available'));
+       && process.stderr.write('[STACK:TRACE]\n' + ((err as any)?.stack ?? 'no stack trace available'));
     return false;
   }
 };
@@ -195,7 +265,7 @@ const parseArgs = () => {
                         Reads from stdin (piped) if no input positional/option defined
   -o, --output  <file>  Specify the output; defaults to stdout
   -l, --lang     <ext>  Set language syntax for comments (e.g: 'js', 'py', 'html')
-      --overwrite       Overwrite the input file (DANGER_ZONE)
+      --overwrite       Overwrite input if: no output or the same (DANGER_ZONE)
 
   -n, --dry-run         Perform a dry run (without writing to a file) & print preview
   -v, --verbose         Enable verbose logging for debugging
@@ -216,18 +286,22 @@ const parseArgs = () => {
   }
 
   if (ctx.version) {
-    std.printf(getMeta().version);
+    // ###[IF]node=1;sed=/process.stdout.write/std.out.puts/;
+    process.stdout.write(getMeta().version);
     return true;
   }
 
   const ctxKeys = Object.keys(ctx);
   const optKeys = Object.keys(DEFAULT_OPTIONS);
-  const argv = ARGV;
+  let argv = ARGV;
   let target: string | null = null;
   let optionTuples = getArgvOptionTuple(argv);
 
-  if (!IS_PIPED && !optionTuples.find(([k]) => k === 'i' || k === 'input')) {
-    target = argv.pop() ?? null;
+  if (!IS_PIPED && !optionTuples.find(([key]) => key === 'i' || key === 'input')) {
+    // attempt to get input positional
+    const pos = getArgvPositional(ctxKeys.concat(optKeys), argv);
+    target = pos?.pop() ?? null;
+    argv = target ? argv.filter(val => val !== target) : argv;
     optionTuples = getArgvOptionTuple(argv);
   }
 
@@ -251,9 +325,9 @@ const parseArgs = () => {
     if (key === 'input' || key === 'i') { istruc.ctx.input = val as string; continue; }
     if (key === 'output' || key === 'o') { istruc.ctx.output = val as string; continue; }
     if (key === 'lang' || key === 'l') {
-      const lang = extensions[val as 'js'] as CommentLang;
+      const lang = extensions[val as 'js'] as CommentRegexOption;
       if (!lang) {
-        std.err.puts(`[ERRO] bad --lang option key of: "${val}"`
+        process.stderr.write(`[ERRO] bad --lang option key of: "${val}"`
         + `\n     > all/langs: ${Object.keys(extensions).join(', ')}`);
         return false;
       }
@@ -266,9 +340,18 @@ const parseArgs = () => {
     if (optKeys.includes(key)) { (istruc.options as any)[key] = val; continue; }
     istruc.directives[key] = val;
   }
-  // const istruc = optionTuples.reduce((acc, [key, val]) => {}, initStruc);
-
   istruc.ctx.input = istruc.ctx.input ?? target ?? null;
+  // ###[IF]node=1;rm=1L;
+  istruc.ctx.input = istruc.ctx.input ? path.resolve(path.normalize(istruc.ctx.input)) : istruc.ctx.input;
+  // ###[IF]node=1;rm=1L;
+  istruc.ctx.output = istruc.ctx.output ? path.resolve(path.normalize(istruc.ctx.output)) : istruc.ctx.output;
+  // ###[IF]node=1;un=comment;
+  /*
+  const [ipath, ierr] = istruc.ctx.input ? os.realpath(istruc.ctx.input) : [istruc.ctx.input, null];
+  istruc.ctx.input = !ierr && ipath ? ipath : istruc.ctx.input;
+  const [opath, oerr] = istruc.ctx.output ? os.realpath(istruc.ctx.output) : [istruc.ctx.output, null];
+  istruc.ctx.output = !oerr && opath ? opath : istruc.ctx.output;
+  */
 
   if (ctx.env || ctx.verbose) {
     console.log(`> ${getMeta().name} (v${getMeta().version})
@@ -284,15 +367,17 @@ const parseArgs = () => {
 };
 
 
-const run = () => {
+const run = async () => {
   try {
-    const isOk = parseArgs();
-    std.exit(isOk ? 0 : 1);
+    const isOk = await parseArgs();
+    // ###[IF]node=1;sed=/process/std/;
+    process.exit(isOk ? 0 : 1);
   } catch (err) {
     const errMsg = '[FATAL][commentDirective:CLI] '
       + (((err instanceof Error) ? err.message : null) ?? 'unknown error');
-    std.err.puts(errMsg + '\n');
-    std.exit(0);
+    process.stderr.write(errMsg + '\n');
+    // ###[IF]node=1;sed=/process/std/;
+    process.exit(0);
   }
 };
 run();
